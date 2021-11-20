6bd66c408613fa4e48b906e51860122a89ea6fcd
unknown <nicholas.wolf@web.de>
2021-11-20 15:47:58 +0100

    remove dotenv requirement

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flaskext.mysql import MySQL
 import os
 import json
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 app = Flask(__name__)
 mysql = MySQL()

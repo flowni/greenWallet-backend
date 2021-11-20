f8e5978b12a96eb9d51a676a9beba3c14475a803
unknown <nicholas.wolf@web.de>
2021-11-20 19:40:16 +0100

    add endpoint for user details

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 from flaskext.mysql import MySQL
 import os
 import json
@@ -11,17 +11,38 @@ app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
 app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
 mysql.init_app(app)
+conn = mysql.connect()
 
 @app.route("/")
 def hello():
-    print("Handling request to home page.2")
-    return "Hello, Azure!"
+    return render_template("index.html")
 
 @app.route("/purchase")
 def checkout_purchase_from_store():
     # receive a json, barcodes and quantities, user_id
     # update tables
     return
+
+@app.route("/user/<user_id>")
+def get_user_details(user_id):
+    cursor = conn.cursor()
+    cursor.execute("select * from users where id = {}".format(user_id))
+    data = cursor.fetchone()
+    json_dict = {
+        "id": data[0],
+        "name": data[1],
+        "email": data[2],
+        "barcode": data[3],
+        "total_coins_earned": data[4],
+        "balance": data[5]
+    }
+    print(json_dict)
+
+    response = app.response_class(response=json.dumps(json_dict),
+                                status=200,
+                                mimetype='application/json')
+    return response
+    
 
 @app.route("/purchases/:user_id")
 def get_purchases_of_user(user_id):
@@ -30,15 +51,11 @@ def get_purchases_of_user(user_id):
 
 @app.route("/users")
 def get_users():
-    print("read: " + os.getenv('MYSQL_DATABASE_PASSWORD'))
     # return all users
-    conn = mysql.connect()
     cursor = conn.cursor()
-
     cursor.execute("select * from users")
     data = cursor.fetchall()
     print(data)
-    # data = {'name': 'nabin khadka'}
     response = app.response_class(response=json.dumps(data),
                                 status=200,
                                 mimetype='application/json')
